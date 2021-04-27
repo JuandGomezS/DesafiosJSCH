bb9e534109fd2b138f1c47fe1ef2c6c1555e68f0
Juan_GomezS <75592506+LYUJuanete@users.noreply.github.com>
2021-04-26 19:26:35 -0500

    arreglo de tipo de dato

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ let time= prompt("Ingresa el tiempo en el que recorriste dicha distancia en segu
 let ok = confirm("Estás seguro de los datos?")
 
 
-let velocity = distance/time;
+let velocity = parseFloat(distance)/parseFloat(time);
 
-document.write("<br/><br/>La velocidad fué de: " + parseFloat(velocity) + " m/s");
+document.write("<br/><br/>La velocidad fué de: " + velocity + " m/s");
 
 
 
